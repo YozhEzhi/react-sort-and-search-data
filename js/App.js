081dd224801597cd92deae8ec485cea9aead27fe
YozhEzhi <yozhezhi@gmail.com>
2017-01-30 18:40:31 +0300

    Changed dependencies

--- js/App.js
+++ js/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Button from './components/Button';
 
-
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -13,22 +12,8 @@ export default class App extends Component {
 
   updateBtn() {
     const phrases = [
-      'ЖМИ!', 'Не останавливайся!',
-      'У тебя хорошо получается!', 'Красавчик!',
-      'Вот это и есть React!', 'Продолжай!',
-      'Пока ты тут нажимаешь кнопку другие работают!',
-      'Всё хватит!', 'Ну и зачем ты нажал?',
-      'В следующий раз тут будет полезный совет',
-      'Чего ты ждешь от этой кнопки?',
       'Если дойдёшь до тысячи, то сразу научищься реакту',
       'ой, всё!', 'Ты нажал кнопку столько раз, что обязан на ней жениться',
-      'У нас было 2 npm-пакета с реактом, 75 зависимостей от сторонних библиотек, '
-      + '5 npm-скриптов и целое множество плагинов галпа всех сортов и расцветок, '
-      + 'а также redux, jquery, mocha, пачка плагинов для eslint и ингерация с firebase. '
-      + 'Не то что бы это был необходимый набор для фронтенда. Но если начал собирать '
-      + 'вебпаком, становится трудно остановиться. Единственное, что вызывало у меня '
-      + 'опасения - это jquery. Нет ничего более беспомощного, безответственного и испорченного, '
-      + 'чем рядовой верстальщик без jquery. Я знал, что рано или поздно мы перейдем и на эту дрянь.',
       'coub про кота-джедая: http://coub.com/view/spxn',
       'Дальнобойщики на дороге ярости: http://coub.com/view/6h0dy',
       'Реакция коллег на ваш код: http://coub.com/view/5rjjw',
@@ -37,7 +22,7 @@ export default class App extends Component {
     ];
     this.setState({
       count: this.state.count + 1,
-      phrase: phrases[parseInt(Math.random() * phrases.length)]
+      phrase: phrases[parseInt(Math.random() * phrases.length, 10)]
     });
   }
 
@@ -45,7 +30,7 @@ export default class App extends Component {
     return (
       <div className="container app">
         <Button count={this.state.count} update={this.updateBtn.bind(this)} />
-        <p style={{marginTop: 2 + 'rem'}}>{this.state.phrase}</p>
+        <p style={{ marginTop: '2rem' }}>{this.state.phrase}</p>
       </div>
     );
   }
--- js/components/Button.js
+++ js/components/Button.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 
-const Button = ({count, update}) => {
+const Button = ({ count, update }) => {
   return (
-    <button className="btn btn-default" onClick={update}>
+    <button className="btn btn-primary" onClick={update}>
       Кнопка была нажата {count} раз
     </button>
   );
-}
-
+};
 
 export default Button;
